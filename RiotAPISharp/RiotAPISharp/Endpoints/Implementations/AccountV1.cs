eef9b72ff35bc822473ea73125343727a2ce32f1
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 16:23:04 +0200

    AccountV1 endpoint interface and class added
    AccountV1 endpoint allows to get data of Valorant account.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/AccountV1.cs
@@ -0,0 +1,23 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.AccountV1;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class AccountV1 : EndpointBase, IAccountV1
+    {
+        private const string accountsByPuuidUrl = "{0}/riot/account/v1/accounts/by-puuid/{1}";
+
+        public AccountV1(string key) : base(key)
+        {
+
+        }
+
+        public AccountDTO GetAccountsByPuuid(Region region, string puuid)
+        {
+            string url = string.Format(accountsByPuuidUrl, region.ToString(), puuid);
+
+            return GetObjectOfType<AccountDTO>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/IAccountV1.cs
@@ -0,0 +1,10 @@
+﻿using RiotAPISharp.Model.AccountV1;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface IAccountV1
+    {
+        public AccountDTO GetAccountsByPuuid(Region region, string puuid);
+    }
+}
