95a706f4db1700d26cdbd3f734308a012974ad8c
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 15:36:10 +0200

    Queue class added
    Contains string value which describes queue type.

---
+++ RiotAPISharp/RiotAPISharp/Utilities/Queue.cs
@@ -0,0 +1,20 @@
+﻿namespace RiotAPISharp.Utilities
+{
+    public class Queue
+    {
+        private string Value { get; set; }
+
+        public static readonly Queue RankedSolo5x5 = new Queue("RANKED_SOLO_5x5");
+        public static readonly Queue RankedFlex5x5 = new Queue("RANKED_FLEX_SR");
+
+        private Queue(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
