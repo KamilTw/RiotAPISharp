4e8ddf313461c16c4e4a9031a00de2818f219eeb
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 16:18:24 +0200

    AccountV1 model class added

---
+++ RiotAPISharp/RiotAPISharp/Model/AccountV1/AccountDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RiotAPISharp.Model.AccountV1
+{
+    public class AccountDTO
+    {
+        public string Puuid { get; set; }
+
+        public string GameName { get; set; }
+
+        public string TagLine { get; set; }
+    }
+}
