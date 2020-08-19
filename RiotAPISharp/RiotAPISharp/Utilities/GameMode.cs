d3554a3f7380a5f56a8a055a11f2a815cdc1f243
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 15:31:21 +0200

    GameMode enum added
    Contains gamemode names with ids.

---
+++ RiotAPISharp/RiotAPISharp/Utilities/GameMode.cs
@@ -0,0 +1,12 @@
+﻿namespace RiotAPISharp.Utilities
+{
+    public enum GameMode
+    {
+        NexusBlitz = 157,
+        NormalDraftPick = 400,
+        RankedSoloDuo = 420,
+        NormalBlindPick = 430,
+        RankedFlex = 440,
+        ARAM = 450
+    }
+}
