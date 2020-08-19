b1f44779df5269702cda7f5b5b7c287df6143418
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 13:32:25 +0200

    EndpointBase abstract class added
    EndpointBase class uses Requester and Deserializer to return object of a given type

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/EndpointBase.cs
@@ -0,0 +1,32 @@
+﻿using RiotAPISharp.Http;
+using RiotAPISharp.Utilities;
+using RiotAPISharp.Utilities.Interfaces;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public abstract class EndpointBase
+    {
+        private IRequester Requester { get; set; }
+        private IDeserializer Deserializer { get; set; }
+
+        protected EndpointBase(string key)
+        {
+            Requester = new Requester(key);
+            Deserializer = new Deserializer();
+        }
+
+        protected T GetObjectOfType<T>(string url)
+        {
+            string responseBody = Requester.CreateGetRequest(url).Result;
+
+            if (responseBody != null)
+            {
+                T obj = Deserializer.DeserializeObject<T>(responseBody);
+
+                return obj;
+            }
+
+            return default;
+        }
+    }
+}
