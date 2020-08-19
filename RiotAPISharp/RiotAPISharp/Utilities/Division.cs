da3242490d20f3f016f051f5004d7da8bd402fd4
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 15:04:37 +0200

    Division class added
    Contains string value which describe division number.

---
+++ RiotAPISharp/RiotAPISharp/Utilities/Division.cs
@@ -0,0 +1,22 @@
+﻿namespace RiotAPISharp.Utilities
+{
+    public class Division
+    {
+        private string Value { get; set; }
+
+        public static readonly Division I = new Division("I");
+        public static readonly Division II = new Division("II");
+        public static readonly Division III = new Division("III");
+        public static readonly Division IV = new Division("IV");
+
+        private Division(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
