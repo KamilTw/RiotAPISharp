9e0f83a4f7b0193c2d99c12f4347badaf1009690
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-19 15:38:53 +0200

    Region class added
    Contains string value which describes routing value for each region.

---
+++ RiotAPISharp/RiotAPISharp/Utilities/Region.cs
@@ -0,0 +1,22 @@
+﻿namespace RiotAPISharp.Utilities
+{
+    public class Region
+    {
+        private string Value { get; set; }
+
+        public static readonly Region AMERICAS = new Region("americas.api.riotgames.com");
+        public static readonly Region ASIA = new Region("asia.api.riotgames.com");
+        public static readonly Region EUROPE = new Region("europe.api.riotgames.com");
+        public static readonly Region SEA = new Region("sea.api.riotgames.com");
+
+        private Region(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
