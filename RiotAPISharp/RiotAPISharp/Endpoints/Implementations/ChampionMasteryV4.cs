29b9062f5e3efd7f8e2b99284f6f4461c7c521f3
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-20 21:30:58 +0200

    ChampionMasteryV4 endpoint interface and class added
    ChampionMasteryV4 endpoint allows to get information about champions' mastery points for each summoner.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/ChampionMasteryV4.cs
@@ -0,0 +1,41 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.ChampionMasteryV4;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class ChampionMasteryV4 : EndpointBase, IChampionMasteryV4
+    {
+        private const string championMasteryBySummonerIdUrl = "{0}/lol/champion-mastery/v4/champion-masteries/by-summoner/{1}";
+        private const string championMasteryChampionIdIdUrl = "{0}/lol/champion-mastery/v4/champion-masteries/by-summoner/{1}/by-champion/{2}";
+        private const string totalChampionMasteryScoreBySummonerIdUrl = "{0}/lol/champion-mastery/v4/scores/by-summoner/{1}";
+
+
+        public ChampionMasteryV4(string key) : base(key)
+        {
+
+        }
+
+        public IEnumerable<ChampionMasteryDTO> GetChampionMasteryBySummonerId(Platform platform, string summonerId)
+        {
+            string url = string.Format(championMasteryBySummonerIdUrl, platform.ToString(), summonerId);
+
+            return GetObjectOfType<IEnumerable<ChampionMasteryDTO>>(url);
+        }
+
+        public ChampionMasteryDTO GetChampionMasteryBySummonerIdAndChampionId(Platform platform, string championId, string summonerId)
+        {
+            string url = string.Format(championMasteryChampionIdIdUrl, platform.ToString(), summonerId, championId);
+
+            return GetObjectOfType<ChampionMasteryDTO>(url);
+        }
+
+        public int GetTotalChampionMasteryScoreBySummonerId(Platform platform, string summonerId)
+        {
+            string url = string.Format(totalChampionMasteryScoreBySummonerIdUrl, platform.ToString(), summonerId);
+
+            return GetObjectOfType<int>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/IChampionMasteryV4.cs
@@ -0,0 +1,15 @@
+﻿using RiotAPISharp.Model.ChampionMasteryV4;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface IChampionMasteryV4
+    {
+        public IEnumerable<ChampionMasteryDTO> GetChampionMasteryBySummonerId(Platform platform, string summonerId);
+
+        public ChampionMasteryDTO GetChampionMasteryBySummonerIdAndChampionId(Platform platform, string championId, string summonerId);
+
+        public int GetTotalChampionMasteryScoreBySummonerId(Platform platform, string summonerId);
+    }
+}
