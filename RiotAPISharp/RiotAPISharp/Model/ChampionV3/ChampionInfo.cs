e33bc443095b116b4f3e681865eb7e85572a920b
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-20 21:32:18 +0200

    ChampionV3 model class added

---
+++ RiotAPISharp/RiotAPISharp/Model/ChampionV3/ChampionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RiotAPISharp.Model.ChampionV3
+{
+    public class ChampionInfo
+    {
+        public int MaxNewPlayerLevel { get; set; }
+
+        public IEnumerable<int> FreeChampionIdsForNewPlayers { get; set; }
+
+        public IEnumerable<int> FreeChampionIds { get; set; }
+    }
+}
