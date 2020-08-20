b390fafcafc3c433a07bc1a5a9b665a1a92b0d18
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-20 21:45:46 +0200

    ClashV1 endpoint interface and class added
    ClashV1 endpoint allows to get tournament, players and teams data of a clash tournament.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/ClashV1.cs
@@ -0,0 +1,56 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.ClashV1;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class ClashV1 : EndpointBase, IClashV1
+    {
+        private const string playersBySummonerIdUrl = "{0}/lol/clash/v1/players/by-summoner/{1}";
+        private const string teamsByTeamIdUrl = "{0}/lol/clash/v1/teams/{1}";
+        private const string tournamentsUrl = "{0}/lol/clash/v1/tournaments";
+        private const string tournamentsByTeamIdUrl = "{0}/lol/clash/v1/tournaments/by-team/{1}";
+        private const string tournamentsByTournamentIdUrl = "{0}/lol/clash/v1/tournaments/{1}";
+
+        public ClashV1(string key) : base(key)
+        {
+
+        }
+
+        public IEnumerable<PlayerDTO> GetPlayersBySummonerId(Platform platform, string summonerId)
+        {
+            string url = string.Format(playersBySummonerIdUrl, platform.ToString(), summonerId);
+
+            return GetObjectOfType<IEnumerable<PlayerDTO>>(url);
+        }
+
+        public TeamDTO GetTeamsByTeamId(Platform platform, string teamId)
+        {
+            string url = string.Format(teamsByTeamIdUrl, platform.ToString(), teamId);
+
+            return GetObjectOfType<TeamDTO>(url);
+        }
+
+        public IEnumerable<TournamentDTO> GetTournaments(Platform platform)
+        {
+            string url = string.Format(tournamentsUrl, platform.ToString());
+
+            return GetObjectOfType<IEnumerable<TournamentDTO>>(url);
+        }
+
+        public TournamentDTO GetTournamentsByTeamId(Platform platform, string teamId)
+        {
+            string url = string.Format(tournamentsByTeamIdUrl, platform.ToString(), teamId);
+
+            return GetObjectOfType<TournamentDTO>(url);
+        }
+
+        public TournamentDTO GetTournamentsByTournamentId(Platform platform, int tournamentId)
+        {
+            string url = string.Format(tournamentsByTournamentIdUrl, platform.ToString(), tournamentId);
+
+            return GetObjectOfType<TournamentDTO>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/IClashV1.cs
@@ -0,0 +1,19 @@
+﻿using RiotAPISharp.Model.ClashV1;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface IClashV1
+    {
+        public IEnumerable<PlayerDTO> GetPlayersBySummonerId(Platform platform, string summonerId);
+
+        public TeamDTO GetTeamsByTeamId(Platform platform, string teamId);
+
+        public IEnumerable<TournamentDTO> GetTournaments(Platform platform);
+
+        public TournamentDTO GetTournamentsByTeamId(Platform platform, string teamId);
+
+        public TournamentDTO GetTournamentsByTournamentId(Platform platform, int tournamentId);
+    }
+}
