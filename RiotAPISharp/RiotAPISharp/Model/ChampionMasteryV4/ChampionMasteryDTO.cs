af2b9a3c95bf26eb1063ab109295760b5b0db613
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-20 21:27:16 +0200

    ChampionMasteryV4 model class added

---
+++ RiotAPISharp/RiotAPISharp/Model/ChampionMasteryV4/ChampionMasteryDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace RiotAPISharp.Model.ChampionMasteryV4
+{
+    public class ChampionMasteryDTO
+    {
+        public long ChampionPointsUntilNextLevel { get; set; }
+
+        public bool ChestGranted { get; set; }
+
+        public long ChampionId { get; set; }
+
+        public long LastPlayTime { get; set; }
+
+        public int ChampionLevel { get; set; }
+
+        public string SummonerId { get; set; }
+
+        public int ChampionPoints { get; set; }
+
+        public long ChampionPointsSinceLastLevel { get; set; }
+
+        public int TokensEarned { get; set; }
+    }
+}
