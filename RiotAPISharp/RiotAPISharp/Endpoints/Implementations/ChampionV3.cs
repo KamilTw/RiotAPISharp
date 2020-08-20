941d50d401831d1b3cdf2d94177060370d41787d
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-20 21:36:00 +0200

    ChampionV3 endpoint interface and class added
    ChampionV3 endpoint allows to get data of a free champion rotations.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/ChampionV3.cs
@@ -0,0 +1,23 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.ChampionV3;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class ChampionV3 : EndpointBase, IChampionV3
+    {
+        private const string championRotationsUrl = "{0}/lol/platform/v3/champion-rotations";
+
+        public ChampionV3(string key) : base(key)
+        {
+
+        }
+
+        public ChampionInfo GetChampionRotations(Platform platform)
+        {
+            string url = string.Format(championRotationsUrl, platform.ToString());
+
+            return GetObjectOfType<ChampionInfo>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/IChampionV3.cs
@@ -0,0 +1,10 @@
+﻿using RiotAPISharp.Model.ChampionV3;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface IChampionV3
+    {
+        public ChampionInfo GetChampionRotations(Platform platform);
+    }
+}
