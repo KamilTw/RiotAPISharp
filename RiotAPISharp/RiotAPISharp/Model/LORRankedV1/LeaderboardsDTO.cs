06844f6492c619bafeff584fadab04105f7dc03b
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-21 20:38:55 +0200

    LORRankedV1 model classes added

---
+++ RiotAPISharp/RiotAPISharp/Model/LORRankedV1/LeaderboardsDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RiotAPISharp.Model.LORRankedV1
+{
+    public class LeaderboardsDTO
+    {
+        public IEnumerable<PlayerDTO> Players { get; set; }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Model/LORRankedV1/PlayerDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RiotAPISharp.Model.LORRankedV1
+{
+    public class PlayerDTO
+    {
+        public string Name { get; set; }
+
+        public int Rank { get; set; }
+
+        public float Lp { get; set; }
+    }
+}
