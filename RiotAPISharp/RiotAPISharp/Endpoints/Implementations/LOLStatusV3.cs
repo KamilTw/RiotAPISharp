835c30e25391f1b6b19292f1d95e4d3569d38d53
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-21 20:30:36 +0200

    LOLStatusV3 endpoint interface and class added
    LOLStatusV3 endpoint provides servers information.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/LOLStatusV3.cs
@@ -0,0 +1,23 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.LOLStatusV3;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class LOLStatusV3 : EndpointBase, ILOLStatusV3
+    {
+        private const string shardDataUrl = "{0}/lol/status/v3/shard-data";
+
+        public LOLStatusV3(string key) : base(key)
+        {
+
+        }
+
+        public ShardStatus GetShardData(Platform platform)
+        {
+            string url = string.Format(shardDataUrl, platform.ToString());
+
+            return GetObjectOfType<ShardStatus>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/ILOLStatusV3.cs
@@ -0,0 +1,10 @@
+﻿using RiotAPISharp.Model.LOLStatusV3;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface ILOLStatusV3
+    {
+        public ShardStatus GetShardData(Platform platform);
+    }
+}
