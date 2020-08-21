8fb1f65bf2199fb326e270003ae222d26591c915
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-21 20:20:19 +0200

    LeagueExpV4 endpoint interface and class added
    LeagueExpV4 endpoint allows to get league data from challenger, grandmaster and master tiers.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/LeagueExpV4.cs
@@ -0,0 +1,24 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.LeagueV4;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class LeagueExpV4 : EndpointBase, ILeagueExpV4
+    {
+        private const string entriesByQueueTierAndDivisionUrl = "{0}/lol/league-exp/v4/entries/{1}/{2}/{3}?page={4}";
+
+        public LeagueExpV4(string key) : base(key)
+        {
+
+        }
+
+        public IEnumerable<LeagueEntryDTO> GetEntriesByQueueTierAndDivision(Platform platform, Queue queue, Tier tier, Division division, int page = 1)
+        {
+            string url = string.Format(entriesByQueueTierAndDivisionUrl, platform.ToString(), queue.ToString(), tier.ToString(), division.ToString(), page);
+
+            return GetObjectOfType<IEnumerable<LeagueEntryDTO>>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/ILeagueExpV4.cs
@@ -0,0 +1,11 @@
+﻿using RiotAPISharp.Model.LeagueV4;
+using System.Collections.Generic;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface ILeagueExpV4
+    {
+        public IEnumerable<LeagueEntryDTO> GetEntriesByQueueTierAndDivision(Platform platform, Queue queue, Tier tier, Division division, int page = 1);
+    }
+}
