31ba779ffb9dedcea9f83813458e66e539dcf948
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-21 20:41:58 +0200

    LORRankedV1 endpoint interface and class added
    LORRankedV1 endpoint provides Legends of Runeterra leaderboards information.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/LORRankedV1.cs
@@ -0,0 +1,23 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.LORRankedV1;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class LORRankedV1 : EndpointBase, ILORRankedV1
+    {
+        private const string accountsByPuuidUrl = "{0}/lor/ranked/v1/leaderboards";
+
+        public LORRankedV1(string key) : base(key)
+        {
+
+        }
+
+        public LeaderboardsDTO GetLeaderboards(Region region)
+        {
+            string url = string.Format(accountsByPuuidUrl, region.ToString());
+
+            return GetObjectOfType<LeaderboardsDTO>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/ILORRankedV1.cs
@@ -0,0 +1,10 @@
+﻿using RiotAPISharp.Model.LORRankedV1;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface ILORRankedV1
+    {
+        public LeaderboardsDTO GetLeaderboards(Region region);
+    }
+}
