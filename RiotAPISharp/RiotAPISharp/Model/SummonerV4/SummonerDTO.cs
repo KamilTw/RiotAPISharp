744db30db875ff7400a211b5310675c0a4ccbddf
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-22 15:38:54 +0200

    SummonerV4 model class added

---
+++ RiotAPISharp/RiotAPISharp/Model/SummonerV4/SummonerDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace RiotAPISharp.Model.SummonerV4
+{
+    public class SummonerDTO
+    {
+        public string AccountId { get; set; }
+
+        public int ProfileIconId { get; set; }
+
+        public long RevisionDate { get; set; }
+
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Puuid { get; set; }
+
+        public long SummonerLevel { get; set; }
+    }
+}
