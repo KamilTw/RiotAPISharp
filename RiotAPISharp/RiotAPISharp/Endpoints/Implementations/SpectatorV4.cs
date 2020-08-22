2ad035ecafc19a8a5b27e85b8bac456d30c0235e
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-22 15:34:43 +0200

    SpectatorV4 endpoint interface and class added
    SpectatorV4 endpoint allows to get active game information.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/SpectatorV4.cs
@@ -0,0 +1,32 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.SpectatorV4;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class SpectatorV4 : EndpointBase, ISpectatorV4
+    {
+        private const string activeGamesBySummonerIdUrl = "{0}/lol/spectator/v4/active-games/by-summoner/{1}";
+        private const string featuredGamesUrl = "{0}/lol/spectator/v4/featured-games";
+
+
+        public SpectatorV4(string key) : base(key)
+        {
+
+        }
+
+        public CurrentGameInfo GetActiveGamesBySummonerId(Platform platform, string summonerId)
+        {
+            string url = string.Format(activeGamesBySummonerIdUrl, platform.ToString(), summonerId);
+
+            return GetObjectOfType<CurrentGameInfo>(url);
+        }
+
+        public FeaturedGames GetFeaturedGames(Platform platform)
+        {
+            string url = string.Format(featuredGamesUrl, platform.ToString());
+
+            return GetObjectOfType<FeaturedGames>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/ISpectatorV4.cs
@@ -0,0 +1,12 @@
+﻿using RiotAPISharp.Model.SpectatorV4;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface ISpectatorV4
+    {
+        public CurrentGameInfo GetActiveGamesBySummonerId(Platform platform, string summonerId);
+
+        public FeaturedGames GetFeaturedGames(Platform platform);
+    }
+}
