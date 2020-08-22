2ae8963689ebeccf3b75cf5d57eb47b253fe30a9
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-22 16:50:21 +0200

    TFTMatchV1 endpoint interface and class added
    TFTMatchV1 endpoint allows to get matches information from Teamfight Tactics.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/TFTMatchV1.cs
@@ -0,0 +1,32 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.TFTMatchV1;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class TFTMatchV1 : EndpointBase, ITFTMatchV1
+    {
+        private const string matchesByPuuidUrl = "{0}/tft/match/v1/matches/by-puuid/{1}/ids?count={2}";
+        private const string matchesByMatchIdUrl = "{0}/tft/match/v1/matches/{1}";
+
+        public TFTMatchV1(string key) : base(key)
+        {
+
+        }
+
+        public IEnumerable<string> GetMatchesByPuuid(Region region, string puuid, int count = 20)
+        {
+            string url = string.Format(matchesByPuuidUrl, region.ToString(), puuid, count);
+
+            return GetObjectOfType<IEnumerable<string>>(url);
+        }
+
+        public MatchDTO GetMatchesByMatchId(Region region, string matchId)
+        {
+            string url = string.Format(matchesByMatchIdUrl, region.ToString(), matchId);
+
+            return GetObjectOfType<MatchDTO>(url);
+        }     
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/ITFTMatchV1.cs
@@ -0,0 +1,13 @@
+﻿using RiotAPISharp.Model.TFTMatchV1;
+using RiotAPISharp.Utilities;
+using System.Collections.Generic;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface ITFTMatchV1
+    {
+        public IEnumerable<string> GetMatchesByPuuid(Region region, string puuid, int count = 20);
+
+        public MatchDTO GetMatchesByMatchId(Region region, string matchId);
+    }
+}
