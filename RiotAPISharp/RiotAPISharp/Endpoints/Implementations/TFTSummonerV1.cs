1a886aede977838d24b2f5dffc69598abbb9d456
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-22 16:51:44 +0200

    TFTSummonerV1 endpoint interface and class added
    TFTSummonerV1 endpoint allows to get summoner data from Teamfight Tactics.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/TFTSummonerV1.cs
@@ -0,0 +1,48 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.SummonerV4;
+using RiotAPISharp.Utilities;
+using System;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class TFTSummonerV1 : EndpointBase, ITFTSummonerV1
+    {
+        private const string summonersByAccountIdUrl = "{0}/tft/summoner/v1/summoners/by-account/{1}";
+        private const string summonersByNameUrl = "{0}/tft/summoner/v1/summoners/by-name/{1}";
+        private const string summonersByPuuidUrl = "{0}/tft/summoner/v1/summoners/by-puuid/{1}";
+        private const string summonersByIdUrl = "{0}/tft/summoner/v1/summoners/{1}";
+
+        public TFTSummonerV1(string key) : base(key)
+        {
+
+        }
+
+        public SummonerDTO GetSummonersByAccountId(Platform platform, string accountId)
+        {
+            string url = string.Format(summonersByAccountIdUrl, platform.ToString(), accountId);
+
+            return GetObjectOfType<SummonerDTO>(url);
+        }
+
+        public SummonerDTO GetSummonersByName(Platform platform, string name)
+        {
+            string url = string.Format(summonersByNameUrl, platform.ToString(), name);
+
+            return GetObjectOfType<SummonerDTO>(url);
+        }
+
+        public SummonerDTO GetSummonersByPuuid(Platform platform, string puuid)
+        {
+            string url = string.Format(summonersByPuuidUrl, platform.ToString(), puuid);
+
+            return GetObjectOfType<SummonerDTO>(url);
+        }
+
+        public SummonerDTO GetSummonersById(Platform platform, string id)
+        {
+            string url = string.Format(summonersByIdUrl, platform.ToString(), id);
+
+            return GetObjectOfType<SummonerDTO>(url);
+        }   
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/ITFTSummonerV1.cs
@@ -0,0 +1,16 @@
+﻿using RiotAPISharp.Model.SummonerV4;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface ITFTSummonerV1
+    {
+        public SummonerDTO GetSummonersByAccountId(Platform platform, string accountId);
+
+        public SummonerDTO GetSummonersByName(Platform platform, string name);
+
+        public SummonerDTO GetSummonersByPuuid(Platform platform, string puuid);
+
+        public SummonerDTO GetSummonersById(Platform platform, string id);
+    }
+}
