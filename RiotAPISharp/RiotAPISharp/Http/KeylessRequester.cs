4f140862ceeb12017ab22fbd3c822dc376853a63
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-24 20:01:14 +0200

    KeylessRequester class added
    KeylessRequester is responsible for creating requests when riot key is not necessary.

---
+++ RiotAPISharp/RiotAPISharp/Http/KeylessRequester.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RiotAPISharp.Http
+{
+    public class KeylessRequester : IRequester
+    {
+        private HttpClient Client { get; set; }
+        private const string httpString = "https://";
+
+        public KeylessRequester()
+        {
+            Client = new HttpClient();
+        }
+
+        public async Task<string> CreateGetRequest(string url)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, httpString + url);
+
+            HttpResponseMessage response = await Client.SendAsync(request);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            if (CheckIfStatusCodeIsOk(response))
+            {
+                return responseBody;
+            }
+
+            return null;
+        }
+
+        private bool CheckIfStatusCodeIsOk(HttpResponseMessage response)
+        {
+            return response.StatusCode == HttpStatusCode.OK;
+        }
+    }
+}
