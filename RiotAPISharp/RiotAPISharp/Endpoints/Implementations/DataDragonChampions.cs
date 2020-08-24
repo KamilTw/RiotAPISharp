cd7fcfe6d83bd5f2c8947ae679d3d886a2b23b04
Kamil <33379395+Rivi96@users.noreply.github.com>
2020-08-24 20:13:22 +0200

    DataDragonChampions endpoint interface and class added
    DataDragonChampions endpoint is responsible for getting detailed champions information.

---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Implementations/DataDragonChampions.cs
@@ -0,0 +1,18 @@
+﻿using RiotAPISharp.Endpoints.Interfaces;
+using RiotAPISharp.Model.DataDragonChampions;
+using RiotAPISharp.Utilities;
+
+namespace RiotAPISharp.Endpoints.Implementations
+{
+    public class DataDragonChampions : EndpointBase, IDataDragonChampions
+    {
+        private const string championsDataUrl = "ddragon.leagueoflegends.com/cdn/{0}/data/{1}/champion.json";   
+
+        public ChampionsData GetChampionsData(string patchNumber, Language language)
+        {
+            string url = string.Format(championsDataUrl, patchNumber, language.ToString());
+
+            return GetObjectOfType<ChampionsData>(url);
+        }
+    }
+}
---
+++ RiotAPISharp/RiotAPISharp/Endpoints/Interfaces/IDataDragonChampions.cs
@@ -0,0 +1,13 @@
+﻿using RiotAPISharp.Model.DataDragonChampions;
+using RiotAPISharp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiotAPISharp.Endpoints.Interfaces
+{
+    public interface IDataDragonChampions
+    {
+        public ChampionsData GetChampionsData(string patchNumber, Language language);
+    }
+}
